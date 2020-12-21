d600970fe5974a3d4b6720356e30d80f94a45dc1
Guangming Wang <guangming.wang@daocloud.io>
2020-12-22 00:44:18 +0800

    增加hystrix监控面板，监控目标服务

--- microservice-04-product-consumer/src/main/java/com/beautytiger/tao/ProductConsumer.java
+++ microservice-04-product-consumer/src/main/java/com/beautytiger/tao/ProductConsumer.java
@@ -1,9 +1,12 @@
 package com.beautytiger.tao;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients(basePackages = {"com.beautytiger.tao"})
 @EnableEurekaClient
@@ -11,5 +14,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class ProductConsumer {
     public static void main(String[] args) {
         SpringApplication.run(ProductConsumer.class, args);
+    }
+
+    // 为了启用hystrix监控
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/actuator/hystrix.stream");
+        return registration;
     }
 }

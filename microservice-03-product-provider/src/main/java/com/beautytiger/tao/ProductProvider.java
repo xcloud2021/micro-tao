23df1a45d4da33f0666bab84f61c03e8f21983c5
Guangming Wang <guangming.wang@daocloud.io>
2020-12-21 22:30:25 +0800

    增加服务注册发现服务eureka的支持

--- microservice-03-product-provider/src/main/java/com/beautytiger/tao/ProductProvider.java
+++ microservice-03-product-provider/src/main/java/com/beautytiger/tao/ProductProvider.java
@@ -3,7 +3,9 @@ package com.beautytiger.tao;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @MapperScan("com.beautytiger.tao.mapper")
 @SpringBootApplication
 public class ProductProvider {

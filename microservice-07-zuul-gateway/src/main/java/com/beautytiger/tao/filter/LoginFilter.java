7bb3f4daca3cb8825adddd5be072733ec061a76a
Guangming Wang <guangming.wang@daocloud.io>
2020-12-22 20:59:36 +0800

    增加zuul网关支持

---
+++ microservice-07-zuul-gateway/src/main/java/com/beautytiger/tao/filter/LoginFilter.java
@@ -0,0 +1,53 @@
+package com.beautytiger.tao.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String token = request.getParameter("token");
+        if (token == null) {
+            logger.warn("token info is null, login check failed");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(200);
+            try {
+                context.getResponse().setContentType("text/html;charset=UTF-8");
+                context.getResponse().getWriter().write("请先登录再操作");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        logger.info("login check passed");
+        return null;
+    }
+}
